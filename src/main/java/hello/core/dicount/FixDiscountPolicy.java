1a438efa27a461eefcee2196db0ebc0d0452ee66
JongyunHa <gkwhddbs77@knou.ac.kr>
2021-07-14 22:37:23 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/dicount/FixDiscountPolicy.java
@@ -0,0 +1,12 @@
+package hello.core.dicount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+    private int discountFixAmount = 1000; // 1000원 할인
+    @Override
+    public int discount(Member member, int price) {
+        return member.getGrade() == Grade.VIP ? discountFixAmount : 0;
+    }
+}
