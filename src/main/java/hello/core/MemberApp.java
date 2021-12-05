eae6d4f6dc460384d93da478592bfc5666528ed3
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-05 13:12:02 +0900

    스프링으로 전환하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,17 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig config = new AppConfig();
-        MemberService memberService = config.memberService();
+//        AppConfig config = new AppConfig();
+//        MemberService memberService = config.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member1 = new Member(1L, "jongyun", Grade.BASIC);
         memberService.join(member1);
 
