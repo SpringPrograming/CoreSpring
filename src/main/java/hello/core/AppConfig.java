348709bdf2bf02183fa38b135befd9cf34b32d5a
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-05 19:57:12 +0900

    다양한 설정 형식 지원 - 자바코드, xml

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,8 +1,7 @@
 package hello.core;
 
-import hello.core.dicount.DiscountPolicy;
-import hello.core.dicount.FixDiscountPolicy;
-import hello.core.dicount.RateDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
