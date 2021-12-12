aa389a8249d6d0078ffada5342108eba224b09a6
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-12 22:07:28 +0900

    생성자 주입을 선택해라!!

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,29 +9,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+  private final MemberRepository memberRepository;
+  private final DiscountPolicy discountPolicy;
 
-    // 생성자가 하나 일경우에는 Autowired 를 생략해도 됨
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+  // 생성자가 하나 일경우에는 Autowired 를 생략해도 됨
+  @Autowired
+  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    System.out.println("memberRepository = " + memberRepository);
+    System.out.println("discountPolicy = " + discountPolicy);
+    this.memberRepository = memberRepository;
+    this.discountPolicy = discountPolicy;
+  }
 
-    @Override
-    public Order createOrder(Long memberId, String itemName, int itemPrice) {
-        Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
+  @Override
+  public Order createOrder(Long memberId, String itemName, int itemPrice) {
+    Member member = memberRepository.findById(memberId);
+    int discountPrice = discountPolicy.discount(member, itemPrice);
 
-        return new Order(memberId, itemName, itemPrice, discountPrice);
-    }
+    return new Order(memberId, itemName, itemPrice, discountPrice);
+  }
 
-    // Test
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
+  // Test
+  public MemberRepository getMemberRepository() {
+    return memberRepository;
+  }
 }
-
