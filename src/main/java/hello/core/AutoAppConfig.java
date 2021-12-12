9594b1497925f2558a53f6f8725521c3975b3520
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-12 15:55:44 +0900

    탐색 위치와 기본 스캔대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-)
-public class AutoAppConfig {
-
-}
+    // Bean 이 등록될때 어디서 부터 찾을지 위치를 명시할수 있음
+    // 지정하지 않으면 기본값은 현재 Annotation 이 붙은 패키지 의 하위를 탐색
+    /* basePackages = "hello.core.member", */
+    excludeFilters =
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+public class AutoAppConfig {}
